9967ee8c8448cb8712a24e5b343fa3150133fd02
KenBarr <ken.barr@solace.com>
2017-03-09 12:05:51 -0500

    Fix the making of swapfile

--- vmr-install.sh
+++ vmr-install.sh
@@ -7,6 +7,8 @@ IMAGE=
 USERNAME=admin
 PASSWORD=admin
 LOG_FILE=install.log
+SWAP_FILE=swap
+SOLACE_HOME=`pwd`
 
 case $key in
     -u|--username)
@@ -28,7 +30,7 @@ esac
 shift # past argument or value
 done
 
-echo "`date` Validate we have been passed a VMR image" &> ${LOG_FILE}
+echo "`date` Validate we have been passed a VMR image" &>> ${LOG_FILE}
 # -----------------------------------------------------
 if [ -z "$IMAGE" ]
 then
@@ -39,13 +41,13 @@ else
 fi
 
 
-echo "`date` Get repositories up to date" &> ${LOG_FILE}
+echo "`date` Get repositories up to date" &>> ${LOG_FILE}
 # ---------------------------------------
 
 yum -y update &> ${LOG_FILE}
 yum -y install lvm2 &> ${LOG_FILE}
 
-echo "`date` Set up Docker Repository" &> ${LOG_FILE}
+echo "`date` Set up Docker Repository" &>> ${LOG_FILE}
 # -----------------------------------
 tee /etc/yum.repos.d/docker.repo <<-EOF
 [dockerrepo]
@@ -57,11 +59,11 @@ gpgkey=https://yum.dockerproject.org/gpg
 EOF
 echo "/etc/yum.repos.d/docker.repo =/n `cat /etc/yum.repos.d/docker.repo`" 
 
-echo "`date` Intall Docker" &> ${LOG_FILE}
+echo "`date` Intall Docker" &>> ${LOG_FILE}
 # -------------------------
-yum -y install docker-engine &> ${LOG_FILE}
+yum -y install docker-engine &>> ${LOG_FILE}
 
-echo "`date` Configure Docker as a service" &> ${LOG_FILE}
+echo "`date` Configure Docker as a service" &>> ${LOG_FILE}
 # ----------------------------------------
 mkdir /etc/systemd/system/docker.service.d &> install.log
 tee /etc/systemd/system/docker.service.d/docker.conf <<-EOF 
@@ -69,35 +71,36 @@ tee /etc/systemd/system/docker.service.d/docker.conf <<-EOF
   ExecStart= 
   ExecStart=/usr/bin/dockerd --iptables=false --storage-driver=devicemapper 
 EOF
-echo "/etc/systemd/system/docker.service.d =/n `cat /etc/systemd/system/docker.service.d`" 
+echo "/etc/systemd/system/docker.service.d =/n `cat /etc/systemd/system/docker.service.d`" &>> ${LOG_FILE}
 
-systemctl enable docker &> ${LOG_FILE} 
-systemctl start docker &> ${LOG_FILE}
+systemctl enable docker &>> ${LOG_FILE}
+systemctl start docker &>> ${LOG_FILE}
 
 echo "`date` Set up swap for 4GB machines"
 # ----------------------------------------
-mkdir /var/lib/solace  &> ${LOG_FILE}
-fallocate /var/lib/solace/swap -l 2G &> ${LOG_FILE}
-mkswap -f /var/lib/solace/swap &> ${LOG_FILE}
-chmod 0600 /var/lib/solace/swap &> ${LOG_FILE}
-swapon -f /var/lib/solace/swap &> ${LOG_FILE}
+mkdir ${SOLACE_HOME} &>> ${LOG_FILE}
+dd if=/dev/zero of=${SOLACE_HOME}/${SWAP_FILE} count=2048 bs=1MiB &>> ${LOG_FILE}
+mkswap -f ${SOLACE_HOME}/${SWAP_FILE} &>> ${LOG_FILE}
+chmod 0600 ${SOLACE_HOME}/${SWAP_FILE} &>> ${LOG_FILE}
+swapon -f ${SOLACE_HOME}/${SWAP_FILE} &>> ${LOG_FILE}
+echo "${SOLACE_HOME}/${SWAP_FILE} swap swap sw 0 0" >> /etc/fstab
 
-echo "`date` Pre-Define Solace required infrastructure" &> ${LOG_FILE}
+echo "`date` Pre-Define Solace required infrastructure" &>> ${LOG_FILE}
 # -----------------------------------------------------
-docker volume create --name=jail  &> ${LOG_FILE}
-docker volume create --name=var  &> ${LOG_FILE}
-docker volume create --name=internalSpool  &> ${LOG_FILE}
-docker volume create --name=adbBackup  &> ${LOG_FILE}
-docker volume create --name=softAdb &> ${LOG_FILE}
+docker volume create --name=jail &>> ${LOG_FILE}
+docker volume create --name=var &>> ${LOG_FILE}
+docker volume create --name=internalSpool &>> ${LOG_FILE}
+docker volume create --name=adbBackup &>> ${LOG_FILE}
+docker volume create --name=softAdb &>> ${LOG_FILE}
 
-echo "`date` Get and load the Solace Docker image" &> ${LOG_FILE}
+echo "`date` Get and load the Solace Docker image" &>> ${LOG_FILE}
 # ------------------------------------------------
-wget ${IMAGE}  &> ${LOG_FILE}
-docker load -i soltr*docker.tar.gz  &> ${LOG_FILE}
-docker images &> ${LOG_FILE}
+wget ${IMAGE} &>> ${LOG_FILE}
+docker load -i soltr*docker.tar.gz &>> ${LOG_FILE}
+docker images &>> ${LOG_FILE}
 
 
-echo "`date` Create a Docker instance from Solace Docker image" &> ${LOG_FILE}
+echo "`date` Create a Docker instance from Solace Docker image" &>> ${LOG_FILE}
 # -------------------------------------------------------------
 VMR_VERSION=`docker images | egrep -o [0-9\.]*vmr_docker[\-\.0-9a-z]*`
 
@@ -112,11 +115,11 @@ docker create \
  -v softAdb:/usr/sw/internalSpool/softAdb \
  --env 'username_admin_globalaccesslevel=admin' \
  --env 'username_admin_password=admin' \
- --name=solace solace-app:${VMR_VERSION} &> ${LOG_FILE}
+ --name=solace solace-app:${VMR_VERSION} &>> ${LOG_FILE}
 
-docker ps -a &> ${LOG_FILE}
+docker ps -a &>> ${LOG_FILE}
 
-echo "`date` Construct systemd for VMR" &> ${LOG_FILE}
+echo "`date` Construct systemd for VMR" &>> ${LOG_FILE}
 # --------------------------------------
 tee /etc/systemd/system/solace-docker-vmr.service <<-EOF
 [Unit]
@@ -130,10 +133,10 @@ tee /etc/systemd/system/solace-docker-vmr.service <<-EOF
 [Install]
   WantedBy=default.target
 EOF
-echo "/etc/systemd/system/solace-docker-vmr.service =/n `cat /etc/systemd/system/solace-docker-vmr.service`" 
+echo "/etc/systemd/system/solace-docker-vmr.service =/n `cat /etc/systemd/system/solace-docker-vmr.service`" &>> ${LOG_FILE} 
 
 echo "`date` Start the VMR"
 # --------------------------
-systemctl daemon-reload &> ${LOG_FILE}
-systemctl enable solace-docker-vmr &> ${LOG_FILE}
-systemctl start solace-docker-vmr &> ${LOG_FILE}
+systemctl daemon-reload &>> ${LOG_FILE}
+systemctl enable solace-docker-vmr &>> ${LOG_FILE}
+systemctl start solace-docker-vmr &>> ${LOG_FILE}
